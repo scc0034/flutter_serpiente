a1e6aa4fb40a31707b908561676b415cba9c6d87
Samuel Casal Cantero <scc0034@alu.ubu.es>
2020-06-29 12:01:26 +0200

    Cambio del home_page a statefulwidget, comprobando la caida de github #18

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_snake/src/widget/menu_lateral.dart';
+import 'package:firebase_admob/firebase_admob.dart';
+ 
+// Variable que va a contener el id del dispositivo
+const String testDevice = "TEST_DEVICE_ID";
 
+class HomePage extends StatefulWidget {
+  @override
+  HomePage_State createState() => HomePage_State();
+}
+
+class HomePage_State extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MenuLateral(),
+        appBar: AppBar(
+          title : Text("Home Page"),
+        ),
+        body: Container(
+          
+        )
+    );
+  }
+}
 class HomePage extends StatelessWidget {
+
 
   @override
   Widget build(BuildContext context) {
